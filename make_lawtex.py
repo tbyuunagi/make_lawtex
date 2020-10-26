53635e029802fdc9cda29f588e211962dbe2825b
tbyuunagi <47624553+tbyuunagi@users.noreply.github.com>
2020-10-27 08:42:21 +0900

    .texファイルの作成の際エンコードを指定するように変更

--- make_lawtex.py
+++ make_lawtex.py
@@ -55,6 +55,11 @@ def get_LawContent(get_Law):
     if not os.path.exists('laws/'):
         os.mkdir('laws')
     tree.write('laws/' + LawName + '.xml', encoding='utf-8', xml_declaration=True)
+
+    with open('laws/' + LawName + '.xml',mode='r',encoding='utf-8') as f:
+        text=f.read()
+    with open('laws/' + LawName + '.xml',mode='w',encoding='utf-8') as f:
+        f.write(delete_ruby(text))
     return LawName
 
 # 検索して保存
@@ -75,11 +80,15 @@ def delete_ruby(text):
     text = re.sub('\\<\\/Ruby\\>','',text)
     return text
 
+def delete_new_line(text):
+    text = re.sub('\\n', '', text)
+    return text
+
 # xmlをtexの形式にする。
 def Part_to_tex(f, Partiter):
     for Part in Partiter:
         for i in Part.find('PartTitle').itertext():
-            f.write('\\part*{' + delete_ruby(i) + '}\n\\addcontentsline{toc}{part}{' + delete_ruby(i) + '}\n')
+            f.write('\\part*{' + delete_new_line(i) + '}\n\\addcontentsline{toc}{part}{' + delete_new_line(i) + '}\n')
         if Part.find('.//Chapter') != None:
             Chapter_to_tex(f, Part.findall('Chapter'))
         else:
@@ -89,7 +98,7 @@ def Part_to_tex(f, Partiter):
 def Chapter_to_tex(f, Chapteriter):
     for Chapter in Chapteriter:
         for i in Chapter.find('ChapterTitle').itertext():
-            f.write('\\section*{' + delete_ruby(i) + '}\n\\addcontentsline{toc}{section}{' + delete_ruby(i) + '}\n')
+            f.write('\\section*{' + delete_new_line(i) + '}\n\\addcontentsline{toc}{section}{' + delete_new_line(i) + '}\n')
         if Chapter.find('.//Section') != None:
             Section_to_tex(f, Chapter.findall('Section'))
         else:
@@ -99,7 +108,7 @@ def Chapter_to_tex(f, Chapteriter):
 def Section_to_tex(f, Sectioniter):
     for Section in Sectioniter:
         for i in Section.find('SectionTitle').itertext():
-            f.write('\\subsection*{' + delete_ruby(i) + '}\n\\addcontentsline{toc}{subsection}{' + delete_ruby(i) + '}\n')
+            f.write('\\subsection*{' + delete_new_line(i) + '}\n\\addcontentsline{toc}{subsection}{' + delete_new_line(i) + '}\n')
         if Section.find('.//Subsection') != None:
             Subsection_to_tex(f, Section.findall('Subsection'))
         else:
@@ -109,7 +118,7 @@ def Section_to_tex(f, Sectioniter):
 def Subsection_to_tex(f, Subsectioniter):
     for Subsection in Subsectioniter:
         for i in Subsection.find('SubsectionTitle').itertext():
-            f.write('\\subsubsection*{' + delete_ruby(i) + '}\n\\addcontentsline{toc}{subsubsection}{' + delete_ruby(i) + '}\n')
+            f.write('\\subsubsection*{' + delete_new_line(i) + '}\n\\addcontentsline{toc}{subsubsection}{' + delete_new_line(i) + '}\n')
         if Subsection.find('.//Division') != None:
             Division_to_tex(f, Subsection.findall('Division'))
         else:
@@ -119,7 +128,7 @@ def Subsection_to_tex(f, Subsectioniter):
 def Division_to_tex(f, Divisioniter):
     for Division in Divisioniter:
         for i in Division.find('DivisionTitle').itertext():
-            f.write('\\subsubsubsection*{' + delete_ruby(i) + '}\n{' + delete_ruby(i) + '}\n')
+            f.write('\\subsubsubsection*{' + delete_new_line(i) + '}\n{' + delete_new_line(i) + '}\n')
         Article_to_tex(f, Division.findall('Article'))
     return
 
@@ -128,13 +137,13 @@ def Article_to_tex(f, Articleiter):
         if Article.find('ArticleCaption') != None:
             a = ""
             for i in Article.find('ArticleCaption').itertext():
-                a += delete_ruby(i)
+                a += delete_new_line(i)
             f.write('\\noindent\\hspace{10pt}' + a + '\n')
         para = 1
         for Paragraph in Article.findall('Paragraph'):
             if len(Article.findall('Paragraph')) > 1:
                 if para == 1:
-                    f.write('\\begin{description}\n\\item[' + delete_ruby(Article.find('ArticleTitle').text) + ']')
+                    f.write('\\begin{description}\n\\item[' + delete_new_line(Article.find('ArticleTitle').text) + ']')
                     para += 1
                     Paragraph_to_tex(f, Paragraph)
                 else:
@@ -142,7 +151,7 @@ def Article_to_tex(f, Articleiter):
                     para += 1
                     Paragraph_to_tex(f, Paragraph)
             else:
-                f.write('\\begin{description}\n\\item[' + delete_ruby(Article.find('ArticleTitle').text) + ']')
+                f.write('\\begin{description}\n\\item[' + delete_new_line(Article.find('ArticleTitle').text) + ']')
                 Paragraph_to_tex(f, Paragraph)
         f.write('\\end{description}\n')
     return
@@ -153,7 +162,7 @@ def Paragraph_to_tex(f, Paragraph):
     if Paragraph.find('ParagraphSentence').find('.//Sentence').text == None:                     
         return                                                                                   
     for i in Paragraph.find('ParagraphSentence').findall('Sentence'):                 
-        f.write(delete_ruby(i.text))                                                                  
+        f.write(delete_new_line(i.text))                                                                  
     f.write('\n')                                                                                
     if Paragraph.find('.//Item') != None:                                                        
         f.write('\\begin{description}\n')                                                        
@@ -163,11 +172,11 @@ def Paragraph_to_tex(f, Paragraph):
 
 def Item_to_tex(f, Itemiter):
     for Item in Itemiter:
-        f.write('\item[' + delete_ruby(Item.find('.//ItemTitle').text) + ']')
+        f.write('\item[' + delete_new_line(Item.find('.//ItemTitle').text) + ']')
         if Item.find('.//ItemSentence').find('.//Column') != None:               
             for Column in Item.find('.//ItemSentence').findall('.//Column'):     
                 for i in Column.findall('Sentence'):                             
-                    f.write(delete_ruby(i.text))                                 
+                    f.write(delete_new_line(i.text))                                 
                 f.write('  ')                                                    
             f.write('\n')
             if Item.find('.//Subitem1') != None:
@@ -178,7 +187,7 @@ def Item_to_tex(f, Itemiter):
             if Item.find('.//ItemSentence').find('.//Sentence') == None:
                 break                                                   
             for i in Item.find('.//ItemSentence').findall('Sentence'):  
-                f.write(delete_ruby(i.text))                            
+                f.write(delete_new_line(i.text))                            
             f.write('\n')                                               
             if Item.find('.//Subitem1') != None:
                 f.write('\\begin{description}\n')              
@@ -188,11 +197,11 @@ def Item_to_tex(f, Itemiter):
     
 def Subitem1_to_tex(f, Subitem1iter):
     for Subitem1 in Subitem1iter:
-        f.write('\item[' + delete_ruby(Subitem1.find('Subitem1Title').text) + ']')
+        f.write('\item[' + delete_new_line(Subitem1.find('Subitem1Title').text) + ']')
         if Subitem1.find('.//Subitem1Sentence').find('.//Column') != None:          
             for Column in Subitem1.find('.//Subitem1Sentence').findall('.//Column'):
                 for i in Column.findall('Sentence'):                                
-                    f.write(delete_ruby(i.text))                                    
+                    f.write(delete_new_line(i.text))                                    
                 f.write('  ')                                                       
             f.write('\n')
             if Subitem1.find('.//Subitem2') != None:
@@ -203,7 +212,7 @@ def Subitem1_to_tex(f, Subitem1iter):
             if Subitem1.find('.//Subitem1Sentence').find('.//Sentence') == None:    
                 break                                                               
             for i in Subitem1.find('.//Subitem1Sentence').findall('Sentence'):      
-                f.write(delete_ruby(i.text))                                        
+                f.write(delete_new_line(i.text))                                        
             f.write('\n')
             if Subitem1.find('.//Subitem2') != None:
                 f.write('\\begin{description}\n')
@@ -213,18 +222,18 @@ def Subitem1_to_tex(f, Subitem1iter):
 
 def Subitem2_to_tex(f, Subitem2iter):
     for Subitem2 in Subitem2iter:
-        f.write('\item[' + delete_ruby(Subitem2.find('Subitem2Title').text) + ']')
+        f.write('\item[' + delete_new_line(Subitem2.find('Subitem2Title').text) + ']')
         if Subitem2.find('.//Subitem2Sentence').find('.//Column') != None:
             for Column in Subitem2.find('.//Subitem2Sentence').findall('.//Column'):
                 for i in Column.findall('Sentence'):    
-                    f.write(delete_ruby(i.text))    
+                    f.write(delete_new_line(i.text))    
                 f.write('  ')                       
             f.write('\n')
         else:
             if Subitem2.find('.//Subitem2Sentence').find('.//Sentence') == None:
                 break                                                           
             for i in Subitem2.find('.//Subitem2Sentence').findall('Sentence'):  
-                f.write(delete_ruby(i.text))                                    
+                f.write(delete_new_line(i.text))                                    
             f.write('\n')                                                       
     return
 
@@ -233,8 +242,8 @@ def xml_to_tex(Law_Name):
         return None
     tree = ET.parse('laws/' + Law_Name + '.xml')
     root = tree.getroot()
-    with open('laws/' + Law_Name + '.tex', 'w') as f:
-        f.write('\\documentclass[twocolumn,a4j,10pt]{ltjtarticle}\n\\title{' + delete_ruby(Law_Name) + '}\n\\author{}\n\\date{}\n\\renewcommand{\\baselinestretch}{0.8}\n\\setlength{\columnseprule}{.4pt}\n\\usepackage{enumitem}\n\\setlist[description]{topsep=3pt,parsep=0pt,partopsep=0pt,itemsep=3pt,leftmargin=10pt,labelsep=5pt,labelsep=10pt}\n\\makeatletter\n\\newcommand{\\subsubsubsection}{\\@startsection{paragraph}{4}{\\z@}%\n  {1.0\\Cvs \\@plus.5\\Cdp \\@minus.2\\Cdp}%\n  {.1\\Cvs \\@plus.3\\Cdp}%\n  {\\reset@font\\sffamily\\normalsize}\n}\n\\makeatother\n\\setcounter{secnumdepth}{4}\n\\begin{document}\n\\maketitle\n\\tableofcontents\n')
+    with open('laws/' + Law_Name + '.tex', 'w', encoding='utf-8') as f:
+        f.write('\\documentclass[twocolumn,a4j,10pt]{ltjtarticle}\n\\title{' + delete_new_line(Law_Name) + '}\n\\author{}\n\\date{}\n\\renewcommand{\\baselinestretch}{0.8}\n\\setlength{\columnseprule}{.4pt}\n\\usepackage{enumitem}\n\\setlist[description]{topsep=3pt,parsep=0pt,partopsep=0pt,itemsep=3pt,leftmargin=10pt,labelsep=5pt,labelsep=10pt}\n\\makeatletter\n\\newcommand{\\subsubsubsection}{\\@startsection{paragraph}{4}{\\z@}%\n  {1.0\\Cvs \\@plus.5\\Cdp \\@minus.2\\Cdp}%\n  {.1\\Cvs \\@plus.3\\Cdp}%\n  {\\reset@font\\sffamily\\normalsize}\n}\n\\makeatother\n\\setcounter{secnumdepth}{4}\n\\begin{document}\n\\maketitle\n\\tableofcontents\n')
         if root.find('.//MainProvision').find('.//Part') != None:
             Part_to_tex(f, root.find('.//MainProvision').iter('Part'))
         elif root.find('.//MainProvision').find('.//Chapter') != None:
